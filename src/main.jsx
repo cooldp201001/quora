c89c637ba3be580a59f3623bfa686c5fa6b280ae
cooldp201001 <cooldp201001@gmail.com>
2023-10-26 00:12:30 +0530

    chakra ui uninstall

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
